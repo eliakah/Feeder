9ff678dbcd353f8d88393ffd473106a9b842b0d6
eliakah <kakoue66@students.rowan.edu>
2017-05-13 17:03:41 -0400

    Interface added 2 implementation added >docfeeder >rssfeeder

--- Feed.py
+++ Feed.py
@@ -1,13 +1,28 @@
-# * Created by Eliakah kakou
-# Feed.py
-# This class allows for more functionality in regards
-# to the dictionary entered in the constructor
-
 class Feed:
-    # constructor
+    """
+    `Author`: Eliakah kakou
+
+    This class allows for more functionality in regards
+    to the dictionary entered in the constructor
+    """
+
     def __init__(self, feed):
+        """
+        `Author`: Eliakah kakou
+
+        The constructor 
+
+        `feed`: dict instance containing set of information about feed
+        """
         self.feed = feed
 
-    # This method returns a subset of the dictionary
     def extract(self):
-        return {k: self.feed[k] for k in ('id', 'title', 'link', 'summary')}
+        """
+        `Author`: Eliakah kakou
+
+        This method returns a subset of the dictionary
+        """
+        return {k: self.feed[k] for k in ('id', 'published', 'title', 'link', 'summary')}
+
+    def record_content(self):
+        return self.feed['link'], self.feed['published']
--- Feeder.py
+++ Feeder.py
@@ -1,36 +1,43 @@
-# * Created by Eliakah kakou
-# Feeder.py
-# This class gets an RSS feed and manipulates
-# the data based on the url entered
-
-
-from Feed import Feed
-import feedparser
-import validators
-import sys
-
-
 class Feeder:
-    # constructor
-    def __init__(self, url):
-        self.errMsg = "Invalid Url: Please try again!"
-        self.url = url
-        self.setUrl(self.url)
+    """
+    `Author`: Bill Clark
 
-    # loadFeeds
-    def load_feeds(self):
-        return self.feeds
+    An interface to fulfill a strategy pattern design in the Grapher with feeders.
+    An implementation of this interface will be usable with the Grapher so long as
+    it provides the appropriate returns. 
 
-    # change Url
-    def setUrl(self, newUrl):
-        self.url = newUrl
-        flag = validators.url(self.url)
-        if flag:
-            self.feeds = feedparser.parse(self.url)
-        else:
-            sys.exit(self.errMsg)
+    Feeder instances should be created with a link to some sort of file. The result
+    to come out of it's methods will be used by the Parser Module to generate topics
+    about the information in the Feed. A Feed represents a single article of related
+    data. The implementation should generate Feed objects as defined above in order
+    to be encapsulated. 
+    """
 
-        list = self.feeds['entries']
-        for i in range(len(list)):
-            list[i] = Feed(list[i])
-            self.feeds = list
+    def __init__(self, file):
+        """
+        A feeder should be initialized to read from it's source when commanded to
+        via the load_feeds method. Feeds is a list of Feed objects specifically, 
+        and is interacted with in the feeds generator call. 
+
+        `file`: The file to process later. 
+        """
+        self.feeds = []
+        self.path = file
+
+    def contents(self):
+        """
+        Returns the feeds the Feeder is holding on to via a generator structure.  
+        """
+        for feed in self.feeds:
+            yield feed
+            # feeds = []
+
+    def fetch(self):
+        """
+        Calling this method should retrieve information using the file provided
+        at initialization. Following that, that information will be standardized
+        into a Feed object and stored in the feeds instance variable. This is going
+        to be implemented in very different ways, depending on the file type used. 
+        """
+        pass
+
